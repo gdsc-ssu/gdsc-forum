b5751a96a549c6da8167bb6b606bc26c34da1366
chaeyeon0130 <k84292330@gmail.com>
2024-06-23 13:12:08 +0900

    feat: BaseEntity에 id 필드 추가

--- src/main/java/com/example/gdscforum/common/entity/BaseEntity.java
+++ src/main/java/com/example/gdscforum/common/entity/BaseEntity.java
@@ -2,6 +2,9 @@ package com.example.gdscforum.common.entity;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.EntityListeners;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import jakarta.persistence.MappedSuperclass;
 import lombok.Getter;
 import org.springframework.data.annotation.CreatedDate;
@@ -13,7 +16,11 @@ import java.time.LocalDateTime;
 @Getter
 @MappedSuperclass
 @EntityListeners(AuditingEntityListener.class)
-public abstract class BaseTimeEntity {
+public abstract class BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
     @CreatedDate
     @Column(name = "created_at", nullable = false)
     private LocalDateTime createdAt;
