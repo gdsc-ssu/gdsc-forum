1153801b112ab1921cb77c5a927b8018676bb3f7
Yarrly <74090200+eunsoA@users.noreply.github.com>
2024-07-14 18:39:28 +0900

    [docs] jwt 주석 추가

--- src/main/java/com/example/gdscforum/common/security/configuration/SecurityConfiguration.java
+++ src/main/java/com/example/gdscforum/common/security/configuration/SecurityConfiguration.java
@@ -25,26 +25,37 @@ import org.springframework.security.web.header.writers.frameoptions.XFrameOption
 public class SecurityConfiguration {
     private final JwtVerifier jwtVerifier;
 
+    //비밀번호를 안전하게 해시하는 데 사용되는 BCryptPasswordEncoder를 빈으로 정의합니다.
+    // 사용자 비밀번호를 데이터베이스에 저장하기 전에 해시화합니다.
     @Bean
     public BCryptPasswordEncoder customPasswordEncoder() {
         return new BCryptPasswordEncoder();
     }
 
+
+    // 인증되지 않은 사용자가 보호된 리소스에 접근하려고 할 때 호출됩니다.
+    // JwtAuthenticationEntryPoint를 통해 처리됩니다.
     @Bean
     public AuthenticationEntryPoint authenticationEntryPoint() {
         return new JwtAuthenticationEntryPoint();
     }
 
+    // 인증된 사용자가 허용되지 않은 리소스에 접근하려고 할 때 호출됩니다.
+    // JwtAccessDeniedHandler를 통해 처리됩니다.
     @Bean
     public AccessDeniedHandler accessDeniedHandler() {
         return new JwtAccessDeniedHandler();
     }
 
+    // 각 요청에 대해 JWT 토큰을 검증하는 필터를 정의합니다.
+    // JwtVerifier를 사용하여 토큰의 유효성을 검증합니다.
     @Bean
     public JwtFilter jwtFilter() {
         return new JwtFilter(jwtVerifier);
     }
 
+
+    // 보안 필터 체인을 정의하고, 여러 가지 보안 설정을 적용합니다.
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http.csrf(AbstractHttpConfigurer::disable);
